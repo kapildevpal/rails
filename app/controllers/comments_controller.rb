23555ab196df17ab1c5092cc6a0678ff55791956
kapildevpal <kapildevpalone@gmail.com>
2022-12-05 18:21:19 +0530

    crud updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,19 @@
 class CommentsController < ApplicationController
-    def create
-      @article = Article.find(params[:article_id])
+
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
+  def create
+    @article = Article.find(params[:article_id])
       @comment = @article.comments.create(comment_params)
       redirect_to article_path(@article)
     end
-  
+  def show
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article)
+  end
+    
     private
     def comment_params
       params.require(:comment).permit(:commenter, :body, :status)
